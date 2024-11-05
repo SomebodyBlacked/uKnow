bb7fa2af5a2b98f8adf4f8e289ff1c4e57619f85
Alguien <WhiteComment@protonmail.com>
2024-11-05 01:08:05 +0000

    add loading state to blog fetching and improve UI feedback

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,7 @@ import Link from "next/link";
 
 export default function Home() {
   const [blogs, setBlogs] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchBlogs = async () => {
@@ -19,6 +20,8 @@ export default function Home() {
         setBlogs(blogsData);
       } catch (error) {
         console.error("Error fetching blogs: ", error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -28,15 +31,20 @@ export default function Home() {
   return (
     <div className="">
       <h1 className="text-4xl font-bold text-center">Welcome to uKnow</h1>
-      <ul>
-        {blogs.map(blog => (
-          <Link href={`/blogs/${blog.id}`} key={blog.title}>
-            <li className="text-lg font-semibold">
-              {blog.title}
-            </li>
-          </Link>
-        ))}
-      </ul>
+      <div className="my-3"></div>
+      {loading ? (
+        <p className="text-center">Cargando blogs...</p>
+      ) : (
+        <ul>
+          {blogs.map(blog => (
+            <Link href={`/blogs/${blog.id}`} key={blog.title}>
+              <li className="border rounded-3xl text-lg font-semibold hover:bg-gray-800 p-4 shadow-md">
+                {blog.title}
+              </li>
+            </Link>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
